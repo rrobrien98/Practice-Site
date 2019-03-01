25f7e57468ac2522f82b3895552a3d159ef73686
rrobrien98 <44010005+rrobrien98@users.noreply.github.com>
2019-03-01 13:38:00 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav id="menu" style="display:none">
+  <a href="about.html">About</a> |
+  <a href="tour.html">Tour</a> |
+  <a href="discography.html">Discography</a> |
+  <a href="index.html">Home</a>
+  <button type="button" onClick="hideMenu()">Hide</button>
+</nav>
+<button type="button" onclick="showMenu()">Info</button>
