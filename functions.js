773d6b820dd388cde6722b7c2038afea4ce1aea2
rrobrien98 <44010005+rrobrien98@users.noreply.github.com>
2019-03-08 21:52:41 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,4 +4,10 @@ function showMenu() {
 function hideMenu() {
  document.getElementById("menu").style.display='none';
 }
-
+var bg_colors = ['coral', 'blue', 'yellow'];
+var current = 0;
+setInterval(function(){
+    document.querySelector('body').style.background = bg_colors[current];
+    current++;
+    if (current == bg_colors.length) current = 0;
+}, 5000);
